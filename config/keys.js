eb2d2faa19d1f8665e8e9074ff59d53aaa55f53b
Sanjog <sanjogstha7@gmail.com>
2018-08-10 11:53:01 +0300

    git ignore

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  googleClientID: '139229091584-kl03l7l0k587p47uptbfjd3ioe7po8sn.apps.googleusercontent.com',
-  googleClientSecret: 'RSAYcjzEFOzBGoPNIsLhxd7X',
-  cloud_name: 'media-upload',
-  api_key: 142277824252693,
-  api_secret: 'IK-TTZ2b3LB-3YdeqneDxwuv4ZM'
-}
