2a7afce78694015daf427a490cf8420659d742a3
Sanjog <sanjogstha7@gmail.com>
2018-08-15 10:39:09 +0300

    key management

---
+++ config/keys_prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoDBConnect: process.env.MONGODB_CONNECT,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET'
+}
